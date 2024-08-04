dc930e58fc9286e6099283ca8f1f5a0c78335a3a
regnna <ritmchats@gmail.com>
2024-08-04 20:57:40 +0530

    Color Validators

---
+++ src/validators/option-validator.ts
@@ -0,0 +1,29 @@
+// bg-blue-950 border-blue-950
+// bg-zinc-900 border-zinc-900
+// bg-rose-950 border-rose-950
+// bg-orange-900 border-orange-900
+
+
+export const COLORS =[
+    {
+        label:"Black",
+        value:"black",
+        tw:'zinc-900'
+
+    },
+    {
+        label:"Blue",
+        value:"blue",
+        tw:'blue-950'
+    },
+    {
+        label:"Rose",
+        value:"rose",
+        tw:'rose-950'
+    },
+    {
+        label:"Orange",
+        value:"orange",
+        tw:'orange-900'
+    },
+] as const
