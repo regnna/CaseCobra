6aef061ae56c355eb07bbe72cbefabee0824c36a
regnna <ritmchats@gmail.com>
2025-01-29 14:08:48 +0530

    Configure Design action saving the config form Auth Call

---
+++ src/app/configure/design/action.ts
@@ -0,0 +1,23 @@
+"use server"
+
+import { db } from "@/db"
+import { CaseColor, CaseFinish, CaseMaterial, PhoneModel } from "@prisma/client"
+
+export type SaveConfigArgs={
+    color:CaseColor,
+    finish:CaseFinish,
+    material:CaseMaterial,
+    model:PhoneModel,
+    configId:string
+}
+
+//Remote Procedure call
+export async function saveConfig({color,finish,material,model,configId}:SaveConfigArgs){
+    await db.configuration.update({
+        where:{id:configId},
+        data:{color,finish,material,model},
+
+    })
+
+
+}
