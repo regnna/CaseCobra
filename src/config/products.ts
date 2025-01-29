558b5b255340eb92c0aa641d06aa814b81ad3720
regnna <ritmchats@gmail.com>
2025-01-29 13:15:07 +0530

    material and texture costing

---
+++ src/config/products.ts
@@ -0,0 +1,14 @@
+export const PRODUCT_PRICES={
+    material:{
+        silicone:2_00,
+        polycarbonate:5_00,
+        plastic:0
+    },
+    finish:{
+        smooth:0,
+        textured: 3_00,
+        leather:5_00,
+    },
+}as const
+
+export const BASE_PRICE=5_00
